4ca0ae2ba48b0fd795bc406aea630846dcf24c06
Patricia <patriciaanneu@gmail.com>
2025-09-20 21:38:33 -0400

    run locally

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/patriciaanneu-portfolio/',
-})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/patriciaanneu-portfolio/',
+    plugins: [react(), tailwindcss()],
+  };
+});
